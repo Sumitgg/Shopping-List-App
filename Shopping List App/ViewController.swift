93d3b2ba2a8b51b5f7ac088dcbd2a0fb4918e225
Sumit Ganju <sganju@sumits-macbook-pro.local>
2017-12-06 22:55:13 -0500

    Added the UI Controls and Functionality to the App.
    Also linked all the UI Elements to the app making all the functionality to work.

--- Shopping List App/ViewController.swift
+++ Shopping List App/ViewController.swift
@@ -11,15 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        super.viewDidLoad()       
 
-    override func didReceiveMemoryWarning() {
+      // Storing core data
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
 }
+
 
